99768c0626ef49adc57b049648a9aef7d2d77688
Phonevilai Vongphom <phonevilai@ldblao.la>
2022-05-11 10:02:22 +0700

    hello: changes for v1.0.1

--- main.go
+++ main.go
@@ -1,7 +1,11 @@
 package main
 
-import "github.com/Phonevilai/learnpackage/v1/tockhelloworld"
+import (
+	"github.com/Phonevilai/learnpackage/v1/pkg/test1"
+	"github.com/Phonevilai/learnpackage/v1/tockhelloworld"
+)
 
 func main() {
 	tockhelloworld.Hi()
+	test1.Test1()
 }
